c8ef6c2613930c3fa8ec84b3a3b9f25edb92403b
Vaishnavi Bhosale <vaishnavibhosale5698@gmail.com>
2020-07-03 21:30:10 +0530

    api integration added

--- src/Component/CodeEditor.jsx
+++ src/Component/CodeEditor.jsx
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from "react";
-// import Prism from "prismjs";
 import * as editor from "highlight.js";
 import python from 'highlight.js/lib/languages/python'
 
-editor.registerLanguage('python',python)
+editor.registerLanguage('python', python)
 
- const CodeEditor = props => {
+const CodeEditor = props => {
   const [content, setContent] = useState(props.content);
+  const [codeOutput, setCodeOutput] = useState('')
 
   const handleKeyDown = event => {
     let value = content,
       selStartPos = event.currentTarget.selectionStart;
 
-     console.log(event.currentTarget);
+    console.log(event.currentTarget);
 
     // handle 4-space indent on
     if (event.key === "Tab") {
@@ -28,17 +28,37 @@ editor.registerLanguage('python',python)
     }
   };
 
-  const  updateCodeSyntaxHighlighting = () => {
-        document.querySelectorAll("pre code").forEach(block => {
-         editor.highlightBlock(block);
-          // const newLanguage = require(`highlight.js/lib/languages/python`);
-          // editor.registerLanguage(props.language, newLanguage)
+  const updateCodeSyntaxHighlighting = () => {
+    document.querySelectorAll("pre code").forEach(block => {
+      editor.highlightBlock(block);
+      // const newLanguage = require(`highlight.js/lib/languages/python`);
+      // editor.registerLanguage(props.language, newLanguage)
       //  editor.highlight('python',block);
-     });
+    });
   }
 
-  
-      
+  const handleExecute = () => {
+    fetch('https://api.jdoodle.com/v1/execute', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "clientId": "d7f973e133f3dac779329dffd3344f4e",
+        "clientSecret": "b67c8484ea26ca17f703c931155c03357f641ef205ba477622d0eff6740c47ff",
+        "script": content,
+        "language": "python3",
+        "versionIndex": "3"
+      })
+    }).then(response => response.json())
+      .then(json => {
+        let codeOutputValue = (JSON.stringify(json.output).slice(1, -1)).replace(/\\n/g, '')
+        setCodeOutput(codeOutputValue)
+      })
+      .catch(error => console.log('Authorization failed : ' + error.message));
+  }
+
   useEffect(() => {
     updateCodeSyntaxHighlighting();
   }, []);
@@ -48,34 +68,35 @@ editor.registerLanguage('python',python)
   }, [props.language, content]);
 
   return (
-    <div style={{width : '100%' , display : 'flex', flexDirection : 'row'} }>
-     
-      <div  className="code-edit-container"> 
-      <textarea
-         className="code-input"
-        value={content}
-        onChange={event => setContent(event.target.value)}
-        onKeyDown={handleKeyDown}
-      />
+    <div style={{ width: '100%', display: 'flex', flexDirection: 'row' }}>
 
-      <pre className="code-syntax-edit">
-        <code class = 'python' className={`language-${props.language}`}>{content}</code>
-      </pre>
+      <div className="code-edit-container">
+        <textarea
+          className="code-input"
+          value={content}
+          onChange={event => setContent(event.target.value)}
+          onKeyDown={handleKeyDown}
+        />
 
-      
-      <div style={{marginTop : '80%',borderTop: '2px solid rgb(99, 97, 97)'}}>
-      <button style={{width : '20%', height : '40px', fontSize: 20,marginTop: '10px', marginLeft: '10px'}}>Run Code >> </button> 
+        <pre className="code-syntax-edit">
+          <code class='python' className={`language-${props.language}`}>{content}</code>
+        </pre>
+
+
+        <div style={{ marginTop: '640px', borderTop: '2px solid rgb(99, 97, 97)' }}>
+          <button style={{ width: '150px', height: '40px', fontSize: 20, marginTop: '10px', marginLeft: '10px' }}
+            onClick={handleExecute}>Run Code >> </button>
+        </div>
+
       </div>
 
-      </div>
-      
-      
-       <div className="output-window">
+
+      <div className="output-window">
         <h3>Output :</h3>
-        2000 is a leap year
+        {codeOutput}
         </div>
     </div>
   );
 };
- 
+
 export default CodeEditor;
